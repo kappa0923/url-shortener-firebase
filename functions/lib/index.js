cf49fe3999e113464a9722ce4f263dff7132cc00
kappa0923 <tom.kappa0923@gmail.com>
2018-05-14 00:34:24 +0900

    一通り作成

---
+++ functions/lib/index.js
@@ -0,0 +1,28 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp(functions.config().firebase);
+/**
+ * @desc ショートリクエストをリダイレクト
+ */
+exports.redirectUrl = functions.https.onRequest((request, response) => {
+    const id = request.path.substr(1);
+    const db = admin.firestore();
+    console.log('ID : ', id);
+    response.header('connection', 'keep-alive');
+    db.collection('urls').doc(id).get()
+        .then(doc => {
+        if (!doc.exists) {
+            console.log('Page not found');
+            response.status(404).end();
+        }
+        else {
+            console.log('Redirect to ', doc.data().url);
+            response.redirect(301, doc.data().url);
+        }
+    }).catch(err => {
+        console.log('Error getting document', err);
+    });
+});
+//# sourceMappingURL=index.js.map
---
+++ functions/src/index.ts
@@ -0,0 +1,28 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+admin.initializeApp(functions.config().firebase);
+
+/**
+ * @desc ショートリクエストをリダイレクト
+ */
+export const redirectUrl = functions.https.onRequest((request, response) => {
+  const id = request.path.substr(1);
+  const db = admin.firestore();
+  console.log('ID : ', id);
+
+  response.header('connection', 'keep-alive');
+
+  db.collection('urls').doc(id).get()
+    .then(doc => {
+      if (!doc.exists) {
+        console.log('Page not found');
+        response.status(404).end();
+      } else {
+        console.log('Redirect to ', doc.data().url);
+        response.redirect(301, doc.data().url);
+      }
+    }).catch(err => {
+      console.log('Error getting document', err);
+    });
+});
